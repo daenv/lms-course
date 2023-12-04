7757ecaf6b021fefa8a83a41aa36fc95fed1200c
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-04 17:56:33 +0700

    update navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import {Sidebar} from "./_components/sidebar";
+import {Navbar} from "@/app/(dashboard)/_components/navbar";
 
 const DashBoardLayout = ({children}: { children: React.ReactNode }) => {
     return (
         <div className='h-full'>
-            <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
-                <Sidebar />
+            <div className={'h-[80px] md:pl-56 fixed inset-y-0 w-full z-50'}>
+                <Navbar/>
             </div>
-            {children}
+            <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
+                <Sidebar/>
+            </div>
+            <main className={'md:pl-56 w-full'}>
+                {children}
+            </main>
         </div>
     )
 }
