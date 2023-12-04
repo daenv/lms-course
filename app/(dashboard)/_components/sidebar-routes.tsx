2721cf6a60c6e66e7db76f888463706de248c7fe
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-04 17:03:51 +0700

    update sidebar

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,48 @@
+'use client';
+import {BarChart, Compass, Layout, List} from "lucide-react"
+import {SidebarItem} from "@/app/(dashboard)/_components/sidebar-item";
+import {usePathname} from "next/navigation";
+
+const guestRoutes = [
+    {
+        icon: Layout,
+        label: 'Dashboard',
+        path: '/'
+    },
+    {
+        icon: Compass,
+        label: 'Browse',
+        path: '/search'
+    },
+]
+const teacherRoutes = [
+    {
+        icon: List,
+        label: 'Courses',
+        path: '/teacher/courses'
+    },
+    {
+        icon: BarChart,
+        label: 'Analytics',
+        path: '/teacher/analytics'
+    }
+]
+const SidebarRoutes = () => {
+    const pathname = usePathname()
+    const isTeacherPage = pathname?.includes('/teacher')
+
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes
+    return (
+        <div className='flex flex-col w-full'>
+            {routes.map((route, index) => (
+                <SidebarItem
+                    key={index}
+                    icon={route.icon}
+                    label={route.label}
+                    href={route.path}
+                />
+            ))}
+        </div>
+    )
+}
+export default SidebarRoutes
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    serverRuntimeConfig:{
+        host: 'dev.lms.aneynguci.com'
+    },
+    publicRuntimeConfig:{
+        host: 'dev.lms.aneynguci.com'
+    }
+}
 
 module.exports = nextConfig
