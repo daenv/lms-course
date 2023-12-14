fb5f47c1eda0358acbee6d691119baefc8431e52
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-14 22:22:37 +0700

    update layout

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 const CoursesPage = () => {
     return (
         <div className={'p6'}>
-
             <Link href={'/teacher/create'}>
                 <Button size={'sm'} variant={'ghost'}>
                     Create course
