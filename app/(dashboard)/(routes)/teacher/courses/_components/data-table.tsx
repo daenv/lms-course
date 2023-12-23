9fe6e0de46c00628ccb3a43d6a17fd29e99283e5
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-23 23:48:31 +0700

    list data table in course

--- app/(dashboard)/(routes)/teacher/courses/_components/data-table.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/data-table.tsx
@@ -1,4 +1,7 @@
 'use client'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@/components/ui/table'
 import {
      ColumnDef,
      ColumnFiltersState,
@@ -10,6 +13,8 @@ import {
      getSortedRowModel,
      useReactTable,
 } from '@tanstack/react-table'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
 import React, { useState } from 'react'
 
 
@@ -42,7 +47,61 @@ function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValu
      return (
           <div>
                <div className='flex items-center py-4 justify-between'>
-
+                    <Input placeholder='Filter courses...' value={(table.getColumn('title')?.getFilterValue() as string) ?? ""}
+                         onChange={(event) => table.getColumn('title')?.setFilterValue(event.target.value)} className='max-w-sm' />
+                    <Link href={'/teacher/create'}>
+                         <Button>
+                              <PlusCircle className='h-4 w-4 mr-2' />
+                              New course
+                         </Button>
+                    </Link>
+               </div>
+               <div className='rounded-md border'>
+                    <Table>
+                         <TableHead>
+                              {table.getHeaderGroups().map((headerGroup) => (
+                                   <TableRow key={headerGroup.id}>
+                                        {headerGroup.headers.map((header) => {
+                                             return (
+                                                  <TableHead key={header.id}>
+                                                       {header.isPlaceholder ? null :
+                                                            flexRender(header.column.columnDef.header,
+                                                                 header.getContext())}
+                                                  </TableHead>
+                                             )
+                                        })}
+                                   </TableRow>
+                              ))}
+                         </TableHead>
+                         <TableBody>
+                              {table.getRowModel().rows.length ? (
+                                   table.getRowModel().rows.map((row) => (
+                                        <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                                             {row.getVisibleCells().map((cell) => (
+                                                  <TableCell key={cell.id}>
+                                                       {flexRender(cell.column.columnDef.cell,
+                                                            cell.getContext())}
+                                                  </TableCell>
+                                             ))}
+                                        </TableRow>
+                                   ))
+                              ) : (
+                                   <TableRow>
+                                        <TableCell colSpan={columns.length} className='h-24 text-center'>
+                                             No Results.
+                                        </TableCell>
+                                   </TableRow>
+                              )}
+                         </TableBody>
+                    </Table>
+               </div>
+               <div className='flex items-center justify-end space-x-2 py-4'>
+                    <Button variant='outline' size={'sm'} onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                         Previous
+                    </Button>
+                    <Button variant={'outline'} size={'sm'} onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                         Next
+                    </Button>
                </div>
           </div>
      )
