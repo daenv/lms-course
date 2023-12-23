6b20962718ddc66bf01f4814a79363ae5c639bbc
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-23 22:15:41 +0700

    columns in course

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,30 @@
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import DataTable from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
+
+const CoursesPage = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect('/')
+    }
+    const course = await db.course.findMany({
+        where: {
+            userId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
     return (
         <div className={'p6'}>
-            <Link href={'/teacher/create'}>
-                <Button size={'sm'} variant={'ghost'}>
-                    Create course
-                </Button>
-            </Link>
+            <DataTable columns={columns} data={course} />
         </div>
     )
 }
