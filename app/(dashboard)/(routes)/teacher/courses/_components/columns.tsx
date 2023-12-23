b33e6faa2af50b74720f6afcda71d79c5d60c67a
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-23 17:46:26 +0700

    update course dashboards

---
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { Course } from '@prisma/client'
+import {ColumnDef} from '@tanstack/react-table'
+import React from 'react'
+
+
+function columns() {
+     return (
+          <div>
+
+          </div>
+     )
+}
+
+export default columns
