1224b37fc622e8c9acc1075fd5477bf87afd1a25
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-24 16:10:18 +0700

    button action with edit course

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+interface ActionsProps {
+     disabled: boolean;
+     courseId: string;
+     isPushlished: boolean;
+}
+
+export const Actions = ({ disabled, courseId, isPushlished }: ActionsProps) => {
+     return (
+          <div>
+               
+          </div>
+     )
+}
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,9 @@
 import { Banner } from '@/components/banner/banner'
+import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 import React from 'react'
+import { Actions } from './_components/actions'
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 
@@ -10,14 +12,60 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
      if (!userId) {
           return redirect('/')
      }
+     const course = await db.course.findUnique({
+          where: {
+               id: params.courseId,
+               userId
+          },
+          include: {
+               chapters: {
+                    orderBy: {
+                         position: 'asc'
+                    }
+               },
+               attachments: {
+                    orderBy: {
+                         createdAt: 'desc'
+                    }
+               }
+          }
+     })
 
-     
+     const categories = await db.category.findMany({
+          orderBy: {
+               name: 'asc'
+          }
+     })
+
+     if (!course) {
+          return redirect('/')
+     }
+
+     const requiredFields = [
+          course.title,
+          course.description,
+          course.imageUrl,
+          course.price,
+          course.categoryId,
+          course.chapters.some(chapter => chapter.isPublished)
+     ]
+
+     const totalFields = requiredFields.length
+     const completedFields = requiredFields.filter(Boolean).length
+
+     const completionText = `(${completedFields}/${totalFields}) `
+
+     const isComplete = requiredFields.every(Boolean)
+
+
 
 
 
      return (
           <>
-               <Banner label={"This course is unpublished. It will not be visible to the students."} />
+               {!course.isPublished && (
+                    <Banner label={"This course is unpublished. It will not be visible to the students."} />
+               )}
 
                <div className='p-6'>
                     <div className='flex items-center justify-between'>
@@ -26,9 +74,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                                    Course setup
                               </h1>
                               <span className='text-sm text-slate-700'>
-                                   Complete all fields
+                                   Complete all fields {completionText}
                               </span>
                          </div>
+                         {/* <Actions  /> */}
                     </div>
                </div>
 
