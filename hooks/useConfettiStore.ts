93bf3b1c845089810a2c18196f33502cf52178bd
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-24 16:21:06 +0700

    create hook useConfettiStores

---
+++ hooks/useConfettiStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+type ConfettiStore = {
+     isOpen: boolean;
+     open: () => void;
+     close: () => void;
+};
+
+export const useConfettiStore = create<ConfettiStore>((set) => ({
+     isOpen: false,
+     open: () => set(() => ({ isOpen: true })),
+     close: () => set(() => ({ isOpen: false })),
+}));
