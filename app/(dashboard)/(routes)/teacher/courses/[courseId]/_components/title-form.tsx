f88cbd6b9c90489712556e061b55f33a92c5ca1f
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-25 12:01:12 +0700

    fix title in form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -51,6 +51,7 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
      return (
           <div className='mt-6 border bg-slate-100 rounded-md p-4'>
                <div className='font-medium flex items-center justify-between'>
+                    Course Title
                     <Button onClick={toggleEdit} variant={'ghost'}>
                          {isEditing ? (
                               <>Cancel</>
@@ -87,7 +88,6 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
                                         Save
                                    </Button>
                               </div>
-
                          </form>
                     </Form>
                )}
