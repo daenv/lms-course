1a48d9de29dad0b304f5dc01523cc8612caf48c1
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-25 13:12:53 +0700

    update file-upload component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,106 +1,103 @@
-import { Banner } from '@/components/banner/banner'
-import { db } from '@/lib/db'
-import { auth } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
+import {Banner} from '@/components/banner/banner'
+import {db} from '@/lib/db'
+import {auth} from '@clerk/nextjs'
+import {redirect} from 'next/navigation'
 import React from 'react'
-import { Actions } from './_components/actions'
-import { IconBadge } from '@/components/icon-badge/icon-bage'
-import { LayoutDashboard } from 'lucide-react'
-import { TitleForm } from './_components/title-form'
-import { DescriptionForm } from './_components/description-form'
-import { ImageForm } from './_components/ImageForm'
+import {Actions} from './_components/actions'
+import {IconBadge} from '@/components/icon-badge/icon-bage'
+import {LayoutDashboard} from 'lucide-react'
+import {TitleForm} from './_components/title-form'
+import {DescriptionForm} from './_components/description-form'
+import {ImageForm} from './_components/ImageForm'
 
-const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+const CourseIdPage = async ({params}: { params: { courseId: string } }) => {
 
-     const { userId } = auth()
+    const {userId} = auth()
 
-     if (!userId) {
-          return redirect('/')
-     }
-     const course = await db.course.findUnique({
-          where: {
-               id: params.courseId,
-               userId
-          },
-          include: {
-               chapters: {
-                    orderBy: {
-                         position: 'asc'
-                    }
-               },
-               attachments: {
-                    orderBy: {
-                         createdAt: 'desc'
-                    }
-               }
-          }
-     })
+    if (!userId) {
+        return redirect('/')
+    }
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId,
+            userId
+        },
+        include: {
+            chapters: {
+                orderBy: {
+                    position: 'asc'
+                }
+            },
+            attachments: {
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            }
+        }
+    })
 
-     const categories = await db.category.findMany({
-          orderBy: {
-               name: 'asc'
-          }
-     })
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: 'asc'
+        }
+    })
 
-     if (!course) {
-          return redirect('/')
-     }
+    if (!course) {
+        return redirect('/')
+    }
 
-     const requiredFields = [
-          course.title,
-          course.description,
-          course.imageUrl,
-          course.price,
-          course.categoryId,
-          course.chapters.some(chapter => chapter.isPublished)
-     ]
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished)
+    ]
 
-     const totalFields = requiredFields.length
-     const completedFields = requiredFields.filter(Boolean).length
+    const totalFields = requiredFields.length
+    const completedFields = requiredFields.filter(Boolean).length
 
-     const completionText = `(${completedFields}/${totalFields}) `
+    const completionText = `(${completedFields}/${totalFields}) `
 
-     const isComplete = requiredFields.every(Boolean)
-
-
+    const isComplete = requiredFields.every(Boolean)
 
 
+    return (
+        <>
+            {!course.isPublished && (
+                <Banner label={"This course is unpublished. It will not be visible to the students."}/>
+            )}
 
-     return (
-          <>
-               {!course.isPublished && (
-                    <Banner label={"This course is unpublished. It will not be visible to the students."} />
-               )}
-
-               <div className='p-6'>
-                    <div className='flex items-center justify-between'>
-                         <div className="flex flex-col gap-y-2">
-                              <h1 className='text-2xl font-medium'>
-                                   Course setup
-                              </h1>
-                              <span className='text-sm text-slate-700'>
+            <div className='p-6'>
+                <div className='flex items-center justify-between'>
+                    <div className="flex flex-col gap-y-2">
+                        <h1 className='text-2xl font-medium'>
+                            Course setup
+                        </h1>
+                        <span className='text-sm text-slate-700'>
                                    Complete all fields {completionText}
                               </span>
-                         </div>
-                         <Actions disabled={!isComplete} courseId={params.courseId} isPublished={course.isPublished} />
                     </div>
-                    <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
-                         <div>
-                              <div className="flex items-center gap-x-2">
-                                   <IconBadge icon={LayoutDashboard} />
-                                   <h2 className='text-xl'>
-                                        Customize your course
-                                   </h2>
-                              </div>
-                              <TitleForm initialData={course} courseId={params.courseId} />
-                              <DescriptionForm initialData={course} courseId={params.courseId} />
-                              <ImageForm initialData={course} courseId={params.courseId} />
-                         </div>
+                    <Actions disabled={!isComplete} courseId={params.courseId} isPublished={course.isPublished}/>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={LayoutDashboard}/>
+                            <h2 className='text-xl'>
+                                Customize your course
+                            </h2>
+                        </div>
+                        <TitleForm initialData={course} courseId={params.courseId}/>
+                        <DescriptionForm initialData={course} courseId={params.courseId}/>
+                        <ImageForm initialData={course} courseId={params.courseId}/>
                     </div>
-               </div>
+                </div>
+            </div>
 
-          </>
-     )
+        </>
+    )
 }
 
 export default CourseIdPage
---
+++ components/file-upload/file-upload.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+interface FileUploadProps {
+    onChange: (url?: string) => void
+    // endpoint: keyof typeof ourFileRouter
+}
