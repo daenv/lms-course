19b015b0fb6f97f2209d66543faaea8ba44f3260
DaEnV <120397126+daenv@users.noreply.github.com>
2023-12-26 17:24:26 +0700

    update index editor & preview

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/description/index.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/description/index.tsx
@@ -1,16 +1,25 @@
-"use client";
+"use client"
 
-import * as z from "zod";
-import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Pencil } from "lucide-react";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import { useRouter } from "next/navigation";
-import { Chapter } from "@prisma/client";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import * as z from "zod"
+import axios from "axios"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Pencil } from "lucide-react"
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
+import { Chapter } from "@prisma/client"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import {
+     Form,
+     FormControl,
+     FormField,
+     FormItem,
+     FormMessage,
+} from "@/components/ui/form";
+import { Editor } from '@/components/editor'
+import { Preview } from '@/components/preview'
 
 interface ChapterDescriptionFromProps {
      initialData: Chapter
@@ -33,7 +42,16 @@ export const ChapterDescriptionForm = ({ initialData, courseId, chapterId }: Cha
      })
 
      const { isSubmitting, isValid } = form.formState
-     const onSubmit = () =>{}
+     const onSubmit = async (values: z.infer<typeof formSchema>) => {
+          try {
+               await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`, values);
+               toast.success("Chapter updated");
+               toggleEdit();
+               router.refresh();
+          } catch {
+               toast.error("Something went wrong");
+          }
+     }
      return (
           <div className="mt-6 border bg-slate-100 rounded-md p-4">
                <div className='font-medium flex items-center justify-between'>
@@ -56,17 +74,47 @@ export const ChapterDescriptionForm = ({ initialData, courseId, chapterId }: Cha
                          !initialData.description && 'text-slate-500 italic'
                     )}>
                          {!initialData.description && 'No description '}
-                         {initialData.description &&(
-                              // <Preview value={initialData.description}/>
-                              <div>
-                                   init
-                              </div>
-                         ) }
+                         {initialData.description && (
+                              <Preview value={initialData.description} />
+
+                         )}
                     </div>
                )
-
                }
+
+               {isEditing && (
+                    <Form {...form}>
+                         <form
+                              onSubmit={form.handleSubmit(onSubmit)}
+                              className="space-y-4 mt-4"
+                         >
+                              <FormField
+                                   control={form.control}
+                                   name="description"
+                                   render={({ field }) => (
+                                        <FormItem>
+                                             <FormControl>
+                                                  <Editor
+                                                       {...field}
+                                                  />
+                                             </FormControl>
+                                             <FormMessage />
+                                        </FormItem>
+                                   )}
+                              />
+                              <div className="flex items-center gap-x-2">
+                                   <Button
+                                        disabled={!isValid || isSubmitting}
+                                        type="submit"
+                                   >
+                                        Save
+                                   </Button>
+                              </div>
+                         </form>
+                    </Form>
+               )}
           </div>
+
 
      )
 }
